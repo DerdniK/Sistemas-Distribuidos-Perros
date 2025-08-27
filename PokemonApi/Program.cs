8a98cff5b81e2c97c9c5846a23f7330cfe75b082
D3RDN1K <148165896+DerdniK@users.noreply.github.com>
2025-08-26 21:13:45 -0600

    feat: Adding mappers, validators, models & more changes

--- PokemonApi/Program.cs
+++ PokemonApi/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Build.Framework;
 using Microsoft.EntityFrameworkCore;
 using PokemonApi.Infrastructure;
+using PokemonApi.Repositories;
 using PokemonApi.Services;
 using SoapCore;
 
@@ -8,6 +9,7 @@ using SoapCore;
 var builder = WebApplication.CreateBuilder(args); // preparar todo el aplicativo para levantar una app web
 builder.Services.AddSoapCore();
 builder.Services.AddScoped<IPokemonServices, PokemonService>();
+builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 
 builder.Services.AddDbContext<RelationalDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
--- PokemonApi/Services/PokemonService.cs
+++ PokemonApi/Services/PokemonService.cs
@@ -1,11 +1,35 @@
+using System.ServiceModel;
 using PokemonApi.Dtos;
+using PokemonApi.Repositories;
+using PokemonApi.Mappers;
+using PokemonApi.Validators;
 
 namespace PokemonApi.Services;
 
 public class PokemonService : IPokemonServices
 {
-    public Task<PokemonResponseDto> CreatePokemon(CreatePokemonDto pokemon, CancellationToken cancellationToken)
+    private readonly IPokemonRepository _pokemonRepository;
+    public PokemonService(IPokemonRepository pokemonRepository)
     {
-        throw new NotImplementedException();
+        _pokemonRepository = pokemonRepository;
+    }
+    public async Task<PokemonResponseDto> CreatePokemon(CreatePokemonDto pokemonRequest, CancellationToken cancellationToken)
+    {
+        pokemonRequest.ValidateName().ValidateLevel().ValidateType();
+        if (await PokemonAlreadyExists(pokemonRequest.Name, cancellationToken))
+        {
+            throw new FaultException("Pokemon already exists");
+        }
+         
+        var pokemon = await _pokemonRepository.CreateAsync(pokemonRequest.ToModel(), cancellationToken);
+
+
+        return pokemon.ToReponseDto();
+    }
+
+    private async Task<bool> PokemonAlreadyExists(string name, CancellationToken cancellationToken)
+    {
+        var pokemons = await _pokemonRepository.GetByNameAsync(name, cancellationToken);
+        return pokemons is not null;
     }
 }
